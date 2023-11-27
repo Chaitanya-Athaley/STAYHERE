4ead5dcaea348ffb9624e8022d20d9fe0ebc56ee
Chaitanya Athaley <jayeshathaley@gmail.com>
2023-11-28 02:06:03 +0530

    revised all commit

--- SH-User/src/main/java/com/stayhere/user/ShUserApplication.java
+++ SH-User/src/main/java/com/stayhere/user/ShUserApplication.java
@@ -3,7 +3,7 @@ package com.stayhere.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.stayhere.common.SuccessCodes;
+//import com.stayhere.common.SuccessCodes;
 
 @SpringBootApplication
 public class ShUserApplication {
@@ -11,7 +11,7 @@ public class ShUserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShUserApplication.class, args);
 //		String codeMessage = SuccessCodes.getCodeMessage("202");
-		System.out.println("----codeMessage");
+		System.out.println("----codeMessa ge");
 	}
 
 }
