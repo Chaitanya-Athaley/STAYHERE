ccd7c710bfff374242995ba9c77561fdc4eb6b94
Chaitanya Athaley <jayeshathaley@gmail.com>
2023-11-28 18:56:43 +0530

    first commit

---
+++ SH-Dao/src/test/java/com/stayhere/ShDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
